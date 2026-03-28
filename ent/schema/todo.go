8acad38ad8fbd0e3964cd48378b34ec274279ab3
ko-yamada <73684559+ko-yamada@users.noreply.github.com>
2026-03-28 22:05:18 +0900

    Update:アノテーションを追加

---
+++ ent/schema/todo.go
@@ -0,0 +1,40 @@
+package schema
+
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Todo holds the schema definition for the Todo entity.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields of the Todo.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New),
+		field.String("name"),
+		field.String("email").
+			NotEmpty().
+			MaxLen(255),
+		field.Bool("done").
+			Default(false),
+	}
+}
+
+// Edges of the Todo.
+func (Todo) Edges() []ent.Edge {
+	return nil
+}
+
+func (Todo) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate(), entgql.MutationUpdate()),
+	}
+}
