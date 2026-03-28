20226b437213db8c12cb8e2f9987dd1bf2888b26
ko-yamada <73684559+ko-yamada@users.noreply.github.com>
2026-03-28 21:56:28 +0900

    Update:Schema作成

---
+++ ent/schema/todo.go
@@ -0,0 +1,31 @@
+package schema
+
+import (
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+	"github.com/google/uuid"
+)
+
+// Todo holds the schema definition for the Todo entity.
+type Todo struct {
+	ent.Schema
+}
+
+// Fields of the Todo.
+func (Todo) Fields() []ent.Field {
+	return []ent.Field{
+		field.UUID("id", uuid.UUID{}).
+			Default(uuid.New),
+		field.String("name"),
+		field.String("email").
+			NotEmpty().
+			MaxLen(255),
+		field.Bool("done").
+			Default(false),
+	}
+}
+
+// Edges of the Todo.
+func (Todo) Edges() []ent.Edge {
+	return nil
+}
